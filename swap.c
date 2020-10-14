edc254665082bccc68ffeb27d9ca1500a40650f3
pallavi2043 <72869873+pallavi2043@users.noreply.github.com>
2020-10-14 06:56:03 -0700

    Update swap.c

--- swap.c
+++ swap.c
@@ -2,8 +2,10 @@
 int main()
 {
 	int a,b,c;
-	printf("Enter the value of a and b");
-	scanf("%d%d",&a,&b);
+	printf("Enter the value of a ");
+	scanf("%d",&a,);
+	printf("enter the value of b");
+	scanf("%d",&b);
 	c=a;
 	a=b;
 	b=c;
